290de0829751e4698a736cd9b44499462accf16d
andrea scacchi <andrea.scacchi@live.com>
2022-07-01 12:08:19 +0200

    Initial commit

---
+++ googleFaq.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Riscrivere questa pagina del sito google
+https://policies.google.com/faq.
+*/
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google Faq</title>
+</head>
+<body>
+    
+    
+</body>
+</html>
